a6b7a89ec67b4f1b21cbd0a8bf4b3f6e9ff16129
José Manuel Cortes Cerón <230110688@itsoeh.edu.mx>
2024-12-13 20:56:43 -0600

    Update: Add favicon

--- js/welcome.js
+++ js/welcome.js
@@ -1,12 +1,12 @@
 // Wait for the page to fully load
 window.addEventListener("load", () => {
-    // After a small delay, hide the loading screen and show the content
-    setTimeout(() => {
-        // Hide the loading screen
-        document.getElementById("loading").style.opacity = "0"; // Make it invisible
-        document.getElementById("loading").style.visibility = "hidden"; // Makes it invisible without removing it
+  // After a small delay, hide the loading screen and show the content
+  setTimeout(() => {
+    // Hide the loading screen
+    document.getElementById("loading").style.opacity = "0"; // Make it invisible
+    document.getElementById("loading").style.visibility = "hidden"; // Makes it invisible without removing it
 
-        // Show the main content
-        document.getElementById("main-content").style.display = "block"; // Display content
-    }, 2000); // 2 seconds to show the loading screen
+    // Show the main content
+    document.getElementById("main-content").style.display = "block"; // Display content
+  }, 2000); // 2 seconds to show the loading screen
 });
